b31f3aba1b0822205e5a64b2552d3f6f54138dd5
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2023-01-06 16:18:42 +0530

    Create 22sorttuplebyfloat.py

---
+++ 22sorttuplebyfloat.py
@@ -0,0 +1,13 @@
+''' Write a Python program to sort a tuple by its float element. 
+Sample data: [('item1', '12.20'), ('item2', '15.10'), ('item3', '24.5')]
+Expected Output: [('item3', '24.5'), ('item2', '15.10'), ('item1', '12.20')]'''
+aList = [('item1', '12.20'), ('item2', '15.10'), ('item3', '24.5')]
+fList = []
+finalList = []
+for i in range(0, len(aList)):
+    fList.append(aList[i][1])
+fsList = sorted(fList)
+for i in range(0, len(fsList)):
+    if fsList[i] == aList[i][1]:
+        finalList.append(aList[i])
+print(finalList)
