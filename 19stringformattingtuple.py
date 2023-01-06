7f56751f4d22c6be40f18d07b88f3af99b0c1405
Daniel Chakraborty <121420532+dansierrasam79@users.noreply.github.com>
2023-01-06 16:04:16 +0530

    Create 19stringformattingtuple.py

---
+++ 19stringformattingtuple.py
@@ -0,0 +1,5 @@
+''' Write a Python program to print a tuple with string formatting. 
+Sample tuple : (100, 200, 300)
+Output : This is a tuple (100, 200, 300)'''
+aTuple = (100, 200, 300)
+print("This is a tuple (%d, %d, %d)" %(aTuple[0], aTuple[1], aTuple[2]))
